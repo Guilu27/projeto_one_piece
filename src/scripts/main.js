fe188bfa7c5afbf1c3827632f26045cf99be2ec0
Guilherme <guilherme@exemplo.com>
2023-09-06 20:55:18 -0300

    secao actors feita

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,66 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
-  const questions = document.querySelectorAll("[data-faq-question]");
 
-  const heroSection = document.querySelector(".hero");
-  const heroHeight = heroSection.clientHeight;
-
-  window.addEventListener("scroll", function () {
-    const currentPosition = window.scrollY;
-
-    if (currentPosition < heroHeight) {
-      hiddenHeaderElements();
-    } else {
-      showsHeaderElements();
-    }
-  });
-
-  // Tabs de programacoes
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (button) {
       const tabTarget = button.target.dataset.tabButton;
       const tab = document.querySelector(`[data-tab-id=${tabTarget}]`);
       hideAllTabs();
-      activeButtonRemove();
-      tab.classList.add("shows__list--is-active");
-      button.target.classList.add("shows__tabs__button--is-active");
+      tab.classList.add("actors__content--is-active");
     });
-  }
-
-  // Accordion secao FAQ
-  for (let i = 0; i < questions.length; i++) {
-    questions[i].addEventListener("click", openOrCloseQuestion);
   }
 });
 
-function hiddenHeaderElements() {
-  const header = document.querySelector("header");
-  header.classList.add("header--is-hidden");
-}
-
-function showsHeaderElements() {
-  const header = document.querySelector("header");
-  header.classList.remove("header--is-hidden");
-}
-
-function openOrCloseQuestion(e) {
-  const className = "faq__questions__item--is-open";
-  const parentElement = e.target.parentNode;
-
-  parentElement.classList.toggle(className);
-}
-
-function activeButtonRemove() {
-  const buttons = document.querySelectorAll("[data-tab-button]");
-
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].classList.remove("shows__tabs__button--is-active");
-  }
-}
-
 function hideAllTabs() {
-  const tabsContainer = document.querySelectorAll("[data-tab-id]");
-  for (let i = 0; i < tabsContainer.length; i++) {
-    tabsContainer[i].classList.remove("shows__list--is-active");
+  const tabs = document.querySelectorAll("[data-tab-id]");
+
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("actors__content--is-active");
   }
 }
