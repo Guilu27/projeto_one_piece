db12f9555515f7ee7b71cf737f920c6f80e75657
Guilherme <guilherme@exemplo.com>
2023-09-08 13:36:01 -0300

    secao actors refeita

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
     buttons[i].addEventListener("click", function (button) {
       const tabTarget = button.target.dataset.tabButton;
       const tab = document.querySelector(`[data-tab-id=${tabTarget}]`);
+
       hideAllTabs();
-      tab.classList.add("actors__content--is-active");
+
+      tab.classList.add("actors__content__tab--is-active");
+
+      const activeButton = document.querySelector(
+        `[data-tab-button=${tabTarget}]`
+      );
+      const navActors = document.querySelector(".actors__content__tabs");
+
+      removeActiveButtonClass();
+      removeClassTabs();
+
+      activeButton.classList.add("actors__content__tabs__button--is-active");
+      navActors.classList.add(`actors__content__tabs--${tabTarget}`);
     });
   }
 });
@@ -15,6 +28,23 @@ function hideAllTabs() {
   const tabs = document.querySelectorAll("[data-tab-id]");
 
   for (let i = 0; i < tabs.length; i++) {
-    tabs[i].classList.remove("actors__content--is-active");
+    tabs[i].classList.remove("actors__content__tab--is-active");
+  }
+}
+
+function removeActiveButtonClass() {
+  const buttons = document.querySelectorAll("[data-tab-button]");
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("actors__content__tabs__button--is-active");
+  }
+}
+
+function removeClassTabs() {
+  const tabs = ["luffy", "nami", "zoro", "sanji", "usopp"];
+  const navActors = document.querySelector(".actors__content__tabs");
+
+  for (let i = 0; i < tabs.length; i++) {
+    navActors.classList.remove(`actors__content__tabs--${tabs[i]}`);
   }
 }
